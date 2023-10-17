3a38ddd80a9d3d30c33585ed0a8d3938384d80c2
watanaberyunosuke <harrybwatson@protonmail.com>
2023-10-17 22:12:54 +1030

    Add custom component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Greet from './greet'
+import Homepage from './homepage'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Greet />
+      <Homepage />
     </main>
   )
 }
